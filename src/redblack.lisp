a2206ce758402e45785baaaf33de16c5ad174edd
Tobias Wich <tobias.wich@electrologic.org>
2010-02-11 03:02:57 +0100

    finished top-down insertion for red black trees

--- src/redblack.lisp
+++ src/redblack.lisp
@@ -17,11 +17,11 @@
 
 (in-package :cl-treemaps)
 
-;(declaim (optimize (debug 3)))
-(declaim (optimize (debug 0)
-		   (safety 0)
-		   (speed 3)
-		   (compilation-speed 0)))
+(declaim (optimize (debug 3)))
+;; (declaim (optimize (debug 0)
+;; 		   (safety 0)
+;; 		   (speed 3)
+;; 		   (compilation-speed 0)))
 
 (defclass redblack-tree-map (tree-map)
   ((data    :accessor data :initform nil
@@ -41,101 +41,263 @@
 
 
 (defmethod get-tree-entry ((tree redblack-tree-map) key &optional (value nil update?))
-  (let ((cmp (testfun tree))
-	(path))
-    (declare (type function cmp))
+  (let (ret-val ret-status)
+    (labels ((local-< (a b)
+	       (funcall (the function (testfun tree)) a b))
+	     (local-= (a b)
+	       (and (not (local-< a b))
+		    (not (local-< b a))))
 
-    (labels (;; main function for search, insert and update
-	     (local-insert (node direction)
+	     ;; search
+	     (local-search (node)
 	       (if (not node)
-		   (if update?
-		       ;; node not existent, create and insert
-		       (let ((new-node (list key value 'red nil nil)))
-			 (cond ((not path) ; tree is empty
-				(setf (data tree) new-node))
-			       ((eq direction 'left) ; set left parent reference
-				(setf (fourth (first path)) new-node))
-			       ((eq direction 'right) ; set right parent reference
-				(setf (fifth (first path)) new-node)))
-			 ;; new node inserted, call repair function
-			 (repair-insert1 new-node path)
-			 (values value t))
-		       ;; pure search method (can't find node)
-		       (values nil nil))
-		   ;; search in the tree
-		   (let ((stored-key (first node)))
-		     (if update? ; add node to path, so parent is visible
-			 (push node path))
-		     (cond
-		       ;; stored-key = key
-		       ((not (or (funcall cmp key stored-key) (funcall cmp stored-key key)))
-			(if update?
-			    ;; update value
-			    (setf (second node) value))
-			(values (second node) t))
-		       ;; left path
-		       ((funcall cmp key stored-key)
-			(local-insert (fourth node) 'left))
-		       ;; right path
-		       ((funcall cmp stored-key key)
-			(local-insert (fifth node) 'right))))))
+		   ;; no such node
+		   nil
+		   ;; else
+		   (let ((node-key (rb-key node)))
+		     (cond ((local-= key node-key)  ; node found
+			    (setf ret-val (rb-value node))
+			    (setf ret-status t)
+			    nil)
+			   (t
+			    ;; go deeper in the appropriate direction
+			    (local-search (rb-child node (if (local-< node-key key) 'right 'left))))))))
 
 
-	     ;;;; repair functions
-	     ;; repair: parent not existant
-	     (repair-insert1 (node path)
-	       (if (not path)
-		   (setf (third node) 'black)
-		   (repair-insert2 node path)))
+	     ;; update
+	     (local-insert ()
+	       (if (not (data tree))
+		   ;; nothing in this tree yet
+		   (progn
+		     (setf ret-val (rb-value
+				    (setf (data tree) (rb-make-node key value))))
+		     (setf ret-status t))
 
-	     ;; repair: parent is black
-	     (repair-insert2 (node path)
-	       (if (eq (third (first path)) 'red) ; do nothing if black
-		   (repair-insert3 node path)))
+		   ;; else: tree not empty
+		   (let ((head (rb-make-empty-node))
+			 (g) (t-node) ; grandparent & parent
+			 (p) (q) ; iterator & parent
+			 (dir 'left)
+			 (last-dir))
+		     ;; set up values
+		     (setf t-node head)
+		     (setf g nil)
+		     (setf p nil)
+		     (setf (rb-right t-node)
+			   (setf q (data tree)))
 
-	     ;; repair: uncle and parent red
-	     (repair-insert3 (node path)
-	       (let* ((parent (first path))
-		      (grandparent (second path))
-		      (uncle (get-brother parent grandparent)))
-		 (if (and uncle (eq 'red (third uncle))) ; uncle = red
-		     (progn
-		       (setf (third parent) 'black)
-		       (setf (third uncle) 'black)
-		       (setf (third grandparent) 'red)
-		       (repair-insert1 grandparent (cddr path))) ; call first repair function
-		     (repair-insert4 node path))))
+		     ;; search down tree
+		     (loop do
+			  (progn
+			    (cond ((not q)
+				   ;; insert new node
+				   (setf (rb-child p dir)
+					 (setf q (rb-make-node key))))
 
-	     ;; repair: black or no uncle and 
-	     (repair-insert4 (node path)
-	       (let ((parent (first path))
-		     (grandparent (second path)))
-		 (cond
-		   ;; node right to parent and parent left to grandparent
-		   ((and (eq node (fifth parent)) (eq parent (fourth grandparent)))
-		    (rotate-left tree parent grandparent)
-		    (repair-insert5 (fourth node) (append (list node) (cdr path))))
-		   ;; node left to parent and parent right to grandparent
-		   ((and (eq node (fourth parent)) (eq parent (fifth grandparent)))
-		    (rotate-right tree parent grandparent)
-		    (repair-insert5 (fifth node) (append (list node) (cdr path)))))))
+				  ;; color flip
+				  ((and (is-red (rb-left q))
+					(is-red (rb-right q)))
+				   (setf (rb-color q) 'red)
+				   (setf (rb-color (rb-left q)) 'black)
+				   (setf (rb-color (rb-right q)) 'black)))
 
-	     ;; repair: node has black or no uncle and is left child of red father
-	     (repair-insert5 (node path)
-	       (let ((parent (first path))
-		     (grandparent (second path))
-		     (grandgrandparent (third path)))
-		 (setf (third parent) 'black)
-		 (setf (third grandparent) 'red)
-		 (if (and (eq node (fourth parent)) (eq parent (fourth grandparent)))
-		     ;; node is left to parent and parent left to grandparent
-		     (rotate-right tree grandparent grandgrandparent)
-		     ;; node is right to parent and parent right to grandparent
-		     (rotate-left tree grandparent grandgrandparent)))))
+			    ;; fix red violation
+			    (if (and (is-red q) (is-red p))
+				(let ((dir2 (if (eq (rb-right t-node) g) 'right 'left)))
+				  (setf (rb-child t-node dir2)
+					(if (eq q (rb-child p last-dir))
+					    (rb-rotate-single g (not-dir last-dir))
+					    (rb-rotate-double g (not-dir last-dir))))))
 
-      ;; call find/update/insert method
-      (local-insert (data tree) nil))))
+			    ;; this is the node
+			    (if (local-= (rb-key q) key)
+				(progn
+				  (setf ret-val
+					(setf (rb-value q) value))
+				  (setf ret-status t)
+				  (loop-finish)))
 
+			    ;; update helpers
+			    (setf last-dir dir)
+			    (setf dir (if (local-< (rb-key q) key) 'right 'left))
+			    (if g (setf t-node g))
+			    (setf g p)
+			    (setf p q)
+			    (setf q (rb-child q dir))))
+
+		     ;; update root
+		     (setf (data tree) (rb-right head))))
+	       ;; make root black
+	       (setf (rb-color (data tree)) 'black)))
+
+      (cond (update?
+	     (local-insert))
+	    (t
+	     (local-search (data tree)))))
+    (values ret-val ret-status)))
+		     
+
+;;;
+;;; internal helper functions
+;;;
+
+(defun rb-key (node &optional (value nil update?))
+  (if update?
+      (setf (first node) value)
+      (first node)))
+(defsetf rb-key rb-key)
+
+(defun rb-value (node &optional (value nil update?))
+  (if update?
+      (setf (second node) value)
+      (second node)))
+(defsetf rb-value rb-value)
+
+
+(defun rb-color (node &optional (value nil update?))
+  (if update?
+      (setf (third node) value)
+      (third node)))
+(defsetf rb-color rb-color)
+
+(defun is-red (node)
+  (and node
+       (eq (rb-color node) 'red)))
+
+(defun is-black (node)
+  (not (is-red node)))
+
+
+(defun rb-child (node direction &optional (value nil update?))
+  (if (eq direction 'left)
+      ;; get left child
+      (if update?
+	  (setf (fourth node) value)
+	  (fourth node))
+      ;; get right child
+      (if update?
+	  (setf (fifth node) value)
+	  (fifth node))))
+(defsetf rb-child rb-child)
+
+(defun rb-left (node &optional (value nil update?))
+  (if update?
+      (setf (rb-child node 'left) value)
+      (rb-child node 'left)))
+(defsetf rb-left rb-left)
+
+(defun rb-right (node &optional (value nil update?))
+  (if update?
+      (setf (rb-child node 'right) value)
+      (rb-child node 'right)))
+(defsetf rb-right rb-right)
+
+
+(defun not-dir (direction)
+  (if (eq direction 'left)
+      'rigth
+      'left))
+
+
+(defun rb-rotate-single (root direction)
+  (let ((save (rb-child root (not-dir direction))))
+    ;; rotate
+    (setf (rb-child root (not-dir direction)) (rb-child save direction))
+    (setf (rb-child save direction) root)
+    ;; recolor
+    (setf (rb-color root) 'red)
+    (setf (rb-color save) 'black)
+    save))
+
+(defun rb-rotate-double (root direction)
+  (setf (rb-child root (not-dir direction))
+	(rb-rotate-single (rb-child root (not-dir direction)) (not-dir direction)))
+  (rb-rotate-single root direction))
+
+
+(defun rb-make-node (key &optional (value nil))
+  (list key value 'red nil nil))
+
+(defun rb-make-empty-node ()
+  (list nil nil 'red nil nil))
+
+
+(defun rb-set-node (old new)
+  "Overwrite old node with new node. This is needed because pointers are not available."
+  (setf (rb-key old) (rb-key new))
+  (setf (rb-value old) (rb-value new))
+  (setf (rb-color old) (rb-color new))
+  (setf (rb-left old) (rb-left new))
+  (setf (rb-right old) (rb-right new))
+  nil)
+
+
+;;;
+;;; test helpers
+;;;
+
+(defun rb-assert (tree)
+  (labels (
+	   ;; comparison functions
+	   (local-< (a b)
+	     (funcall (the function (testfun tree)) a b))
+	   (local-> (a b)
+	     (local-< b a))
+	   (local-= (a b)
+	     (and (not (local-< a b))
+		  (not (local-< b a))))
+	   (local-<= (a b)
+	     (or (local-< a b)
+		 (local-= a b)))
+	   (local->= (a b)
+	     (or (local-> a b)
+		 (local-= a b)))
+
+	   ;; check function
+	   (local-assert (root)
+		    (let (lh rh)
+		      (if (not root)
+			  (return-from local-assert 1)
+			  ;; else
+			  (let ((ln (rb-left root))
+				(rn (rb-right root)))
+			    ;; consecutive red links
+			    (if (is-red root)
+				(if (or (is-red ln) (is-red rn))
+				    (progn
+				      (print "Red violation")
+				      (return-from local-assert 0))))
+
+			    (setf lh (local-assert ln))
+			    (setf rh (local-assert rn))
+
+			    ;; invalid binary search tree
+			    (if (or (and ln (local->= (rb-key ln) (rb-key root)))
+				    (and rn (local-<= (rb-key rn) (rb-key root))))
+				(progn
+				  (print "Binary tree violation")
+				  (return-from local-assert 0)))
+
+			    ;; black height mismatch
+			    (if (and (not (zerop lh))
+				     (not (zerop rh))
+				     (not (= lh rh)))
+				(progn
+				  (print "Black violation")
+				  (return-from local-assert 0)))
+
+			    ;; only count black links
+			    (if (and (not (zerop lh))
+				     (not (zerop rh)))
+				(return-from local-assert (if (is-red root) lh (1+ lh)))
+				(return-from local-assert 0)))))))
+    ;; call check for the tree
+    (local-assert (data tree))))
+
+
+;;;
+;;; legacy
+;;;
 
 (defun get-brother (node parent)
   (let ((left (fourth parent))
@@ -143,113 +305,3 @@
     (if (eq left node)
 	right
 	left)))
-
-
-(defun rotate-left (tree parent grandparent)
-  (let* ((node (fifth parent))
-	 (left-child (fourth node)))
-    (setf (fourth node) parent) ; move node up and parent down
-    (setf (fifth parent) left-child) ; move left child to place where node was
-    (if grandparent
-	(if (eq (fourth grandparent) parent) ;; parent was left child of grandparent
-	    (setf (fourth grandparent) node)
-	    (setf (fifth grandparent) node))
-	(setf (data tree) node))))
-
-(defun rotate-right (tree parent grandparent)
-  (let* ((node (fourth parent))
-	 (right-child (fifth node)))
-    (setf (fifth node) parent) ; move node up and parent down
-    (setf (fourth parent) right-child) ; move right child to place where node was
-    (if grandparent
-	(if (eq (fourth grandparent) parent) ;; parent was left child of grandparent
-	    (setf (fourth grandparent) node)
-	    (setf (fifth grandparent) node))
-	(setf (data tree) node))))
-
-
-;; (defmethod del-tree-entry ((tree redblack-tree-map) key)
-;;   (let ((cmp (testfun tree))
-;; 	(path))
-;;     (declare (type function cmp))
-
-;;     (labels (;; main function for search and remove
-;; 	     (local-search (node)
-;; 	       (if (not node)
-;; 		   nil    ; no such node found in the tree
-;; 		   (progn ; check out which node to take next
-;; 		     (let ((stored-key (first node)))
-;; 		       (push node path)
-;; 		       (cond
-;; 			 ;; stored-key = key
-;; 			 ((not (or (funcall cmp key stored-key) (funcall cmp stored-key key)))
-;; 			  (local-remove node)
-;; 			 ;; left path
-;; 			 ((funcall cmp key stored-key)
-;; 			  (local-search (fourth node)))
-;; 			 ;; right path
-;; 			 ((funcall cmp stored-key key)
-;; 			  (local-search (fifth node)))))))))
-
-;; 	     ;; find maximum child
-;; 	     (local-max (node)
-;; 	       (cond ((fifth node) ; right child present
-;; 		      (push node path)
-;; 		      (local-max (fifth node)))
-;; 		     (t            ; maximum found
-;; 		      node)))
-;; 	     ;; find minimum child
-;; 	     (local-min (node)
-;; 	       (cond ((fourth node) ; right child present
-;; 		      (push node path)
-;; 		      (local-min (fourth node)))
-;; 		     (t            ; maximum found
-;; 		      node)))
-;; 	     ;; replace node a with node b
-;; 	     (swap-nodes (a b)
-;; 	       (setf (first a) (first b))
-;; 	       (setf (second a) (second b)))
-
-;; 	     ;; remove node
-;; 	     (local-remove (node)
-;; 	       (let (del-node)
-;; 		 (cond
-;; 		   ((fourth node) ; at least one left child present
-;; 		    ;; find maximum starting from left child
-;; 		    (setf del-node (local-max (fourth node)))
-;; 		    ;; swap nodes
-;; 		    (swap-nodes node del-node)
-;; 		    ;; call deletion algorithm
-;; 		    (del-child del-node 'left))
-
-;; 		   ((fifth node) ; at least one right child
-;; 		    ;; find minimum starting from right child
-;; 		    (setf del-node (local-min (fifth node)))
-;; 		    ;; swap nodes
-;; 		    (swap-nodes node del-node)
-;; 		    ;; call deletion algorithm
-;; 		    (del-child del-node 'right))
-
-;; 		   (t             ; no child here, empty tree and I'm done
-;; 		    (clr-tree tree)))
-;; 		 ;; delete node i found
-;; 		 (local-remove-child del-node)
-;; 		 t))
-
-;; 	     (del-child (node child-at)
-;; 	       (let ((child (if (eq child-at 'left) (fourth node) (fifth node))))
-;; 		 (cond (child
-;; 			(swap-nodes node child)
-;; 			(if (eq (third node) 'black)
-;; 			    (if (eq (third child) 'red)
-;; 				(setf (third child) 'black)
-;; 				(repair-delete1 child (append (list node) path)))))
-;; 		       (t ; no child at node
-			
-;; 			))
-;; 		 ;; finnally delete node
-;; 		 (let ((parent (first path)))
-;; 		   (cond (parent ; parent has node right if child-at is left
-			  
-;; 			  )
-;; 			 (t      ; node is root
